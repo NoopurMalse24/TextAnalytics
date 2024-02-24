3cc73cc4ff48572f2b13157c7c3157efa913b2a1
noopur123github <noopurabc123@gmail.com>
2024-02-24 17:55:22 +0530

    Frontend changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import './Navbar.css';
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>Text Analytics</h1>
+      <h1>InsightPlus</h1>
       <ul>
         <li><a href="/">Home</a></li>
         {/* Add more navigation links if needed */}
